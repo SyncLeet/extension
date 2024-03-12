4a76018eb7a721c0c8321f4dfe8e12d00d951625
Sen Feng <senfeng6@gmail.com>
2024-03-12 11:18:05 -0400

    Remove debug log

--- src/popup.js
+++ src/popup.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     let checkbox = document.getElementById('notificationStatus');
-    console.log(checkbox);
+
     // Load the saved state from Chrome storage and set the checkbox accordingly
     chrome.storage.sync.get('notificationStatus', function(data) {
       checkbox.checked = data.notificationStatus;
