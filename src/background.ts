fc8279ca49a22d0dfa01dbcfd219654f23202827
Hao Kang <haok1402@gmail.com>
2024-03-23 16:43:46 -0400

    fix arbitrary refresh problem while chrome bring extension from active to inactive

--- src/background.ts
+++ src/background.ts
@@ -1,11 +1,16 @@
 import { initializeBackground as githubInitialize } from "src/modules/github";
 import { initializeBackground as leetcodeInitialize } from "src/modules/leetcode";
 
-chrome.tabs.query({ url: "https://leetcode.com/*" }, (tabs) => {
-  for (const tab of tabs) {
-    chrome.tabs.reload(tab.id);
-  }
-  githubInitialize().then((octokit) => {
-    leetcodeInitialize(octokit);
+// Reload all LeetCode tabs when the extension is just installed or updated
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.tabs.query({ url: "https://leetcode.com/*" }, (tabs) => {
+    for (const tab of tabs) {
+      chrome.tabs.reload(tab.id);
+    }
   });
+});
+
+// Initialize the background script
+githubInitialize().then((octokit) => {
+  leetcodeInitialize(octokit);
 });
